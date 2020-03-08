4e10210088c7955ab6fc2f9953404269af608e6d
Fi <44496637+litmify@users.noreply.github.com>
2020-03-09 00:00:50 +0900

    Add files via upload

---
+++ precache-manifest.60e3439f72ec50caa5d0e0b71bb26d72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2f24d78cb97bace2111567607528252",
+    "url": "/index.html"
+  },
+  {
+    "revision": "878ced9e731b1ab3dc0e",
+    "url": "/static/css/main.bc9d913b.chunk.css"
+  },
+  {
+    "revision": "3c3c710ed91f34b82878",
+    "url": "/static/js/2.90ed6d0b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.90ed6d0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "878ced9e731b1ab3dc0e",
+    "url": "/static/js/main.e6a78e83.chunk.js"
+  },
+  {
+    "revision": "6d0251a9be205232da35",
+    "url": "/static/js/runtime-main.1a52b2cf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/apply-sparcs/precache-manifest.60234251c8a6be6716a89d39ad4f7d98.js"
+  "/precache-manifest.60e3439f72ec50caa5d0e0b71bb26d72.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/apply-sparcs/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
